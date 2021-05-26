1ce31056fcde8d076e05541c3314898e4a08629c
Sir Kane <ahmadouwalyndiaye@gmail.com>
2021-05-26 16:24:18 +0000

    - Handle image using "eleventy-img" - Add contact info

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,54 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const Image = require("@11ty/eleventy-img");
+const path = require("path");
+
+async function imageShortcode(src, alt, w, h) {
+  let sizes = "(min-width: 1024px) 100vw, 50vw";
+  let srcPrefix = `./img/`;
+  src = srcPrefix + src;
+  console.log(`Generating image(s) from:  ${src}`);
+  if (alt === undefined) {
+    // Throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
+  let metadata = await Image(src, {
+    widths: [600, 900, 1500],
+    formats: ["webp", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./_site/images/",
+    /* =====
+    Now we'll make sure each resulting file's name will 
+    make sense to you. **This** is why you need 
+    that `path` statement mentioned earlier.
+    ===== */
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src);
+      const name = path.basename(src, extension);
+      return `${name}-${width}w.${format}`;
+    },
+  });
+  let lowsrc = metadata.jpeg[0];
+  return `<picture>
+    ${Object.values(metadata)
+      .map((imageFormat) => {
+        return `  <source type="${
+          imageFormat[0].sourceType
+        }" srcset="${imageFormat
+          .map((entry) => entry.srcset)
+          .join(", ")}" sizes="${sizes}">`;
+      })
+      .join("\n")}
+    <img
+      src="${lowsrc.url}"
+      width="${w || lowsrc.width}"
+      height="${h || lowsrc.height}"
+      alt="${alt}"
+      loading="lazy"
+      decoding="async">
+  </picture>`;
+}
 
 module.exports = function (eleventyConfig) {
   // Add plugins
@@ -93,6 +141,11 @@ module.exports = function (eleventyConfig) {
     ui: false,
     ghostMode: false,
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+  eleventyConfig.addLiquidShortcode("image", imageShortcode)
+  // === Liquid needed if `markdownTemplateEngine` **isn't** changed from Eleventy default
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode)
 
   return {
     // Control which files Eleventy will process
