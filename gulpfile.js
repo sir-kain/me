4b26e3972f412d35a95c877807a0c487ed352e2c
Sir Kane <ahmadouwalyndiaye@gmail.com>
2021-05-26 12:04:36 +0000

    Used gulp to complie sass

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { src, series, watch, parallel, dest } = require("gulp");
+const rimraf = require("rimraf");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const cleanCSS = require("gulp-clean-css");
+const minify = require("gulp-minify");
+
+function style(cb) {
+  src("css/*.scss", {
+    sourcemaps: true,
+  })
+    .pipe(sass())
+    .on("error", function (error) {
+      sass.logError.call(this, error);
+      // return process.exit(1);
+    })
+    .pipe(postcss([autoprefixer()]))
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write(""))
+    .pipe(dest("_site/css/"));
+  cb();
+}
+
+function watchStyle(cb) {
+  watch("css/*.scss", style);
+  cb();
+}
+
+function deleteCss(cb) {
+  rimraf("_site/css", (err) => console.error(err));
+  cb();
+}
+
+exports.default = series(deleteCss, style, watchStyle);
